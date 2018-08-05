21642bcc90326a88a725a35f9039bd5245c60ece
张威 <zw900808@gmail.com>
2018-08-05 11:17:56 +0800

    配置单点登录

---
+++ awbeci-sso-server/src/main/java/com/awbeci/sso/SsoServerApp.java
@@ -0,0 +1,11 @@
+package com.awbeci.sso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsoServerApp {
+    public static void main(String[] args) {
+        SpringApplication.run(SsoServerApp.class, args);
+    }
+}
---
+++ awbeci-sso-server/src/main/java/com/awbeci/sso/server/SsoAuthorizationServerConfig.java
@@ -0,0 +1,57 @@
+package com.awbeci.sso.server;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class SsoAuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("awbeci1")
+                .secret("awbeci-secret1")
+                .authorizedGrantTypes("authorization_code","refresh_code")
+                .scopes("all")
+                .and()
+                .withClient("awbeci2")
+                .secret("awbeci-secret2")
+                .authorizedGrantTypes("authorization_code","refresh_code")
+                .scopes("all");
+    }
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(jwtTokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter());
+    }
+
+    @Override
+    public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+        // 受权表达式
+        // 经过token获取的时候要经过身份认证
+        // 用户认证-->获取token-->授权访问
+        security.tokenKeyAccess("isAuthenticated()");
+    }
+
+    @Bean
+    public TokenStore jwtTokenStore(){
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter(){
+        JwtAccessTokenConverter jwtAccessTokenConverter = new JwtAccessTokenConverter();
+        jwtAccessTokenConverter.setSigningKey("awbeci");
+        return jwtAccessTokenConverter;
+    }
+}
